a60f6b9e1b60c9618dd98f566e07da99481b0e99
soga5348 <96822881+soga5348@users.noreply.github.com>
2022-08-10 18:45:15 +0900

    Add files via upload

---
+++ note.py
@@ -0,0 +1,20 @@
+# 3/25 scrapingについての学習
+# seleniumの使い方解説
+# 参考url
+https://ai-inter1.com/python-selenium/#st-toc-h-18
+https://qiita.com/centerfield77/items/0aebd6ed452b278d872c
+https://pypi.org/project/webdriver-manager/
+https://python-auto.com/2020/06/07/【python】selenium-webdriverderプロセスまで終了させる%E3%80%82/
+https://rseiub.com/75
+https://techacademy.jp/magazine/18963
+http://yay.cla.kobe-u.ac.jp/~jm/edu/2016/PB/python3/python3ex01.html
+https://scraping-for-beginner.herokuapp.com/login_page]
+
+https://qiita.com/shobota/items/5324ca810b3848c4fd0f
+
+# アクティビティモニタで起動中のツール？がわかる
+# フィードバック→次回までにpython練習問題やっておくのと、seleniumの勉強進める
+
+
+
+
